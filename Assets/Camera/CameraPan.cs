db7094c2e0b9b4306113147646fae17eecb7b815
Trimadzo <trimadzo@gmail.com>
2018-05-17 12:20:10 +0200

    Selfie Stick Camera

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        // print("RHoriz " + Input.GetAxis("RHoriz"));
+        // print("RVert " + Input.GetAxis("RVert"));
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(player.transform);
+    }
+
+}
---
+++ Assets/Camera/SelfieStick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfieStick : MonoBehaviour
+{
+
+    public float panSpeed = 3f;
+
+    private GameObject player;
+    private Vector3 armRotation;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        armRotation = transform.rotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        armRotation.y += Input.GetAxis("RHoriz") * panSpeed;
+        armRotation.x += Input.GetAxis("RVert") * panSpeed;
+
+        transform.position = player.transform.position;
+        transform.rotation = Quaternion.Euler(armRotation);
+    }
+}
